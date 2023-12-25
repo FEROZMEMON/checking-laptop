e99941b3e24adf2eb7378bbadec4b44372ca3fc7
FEROZMEMON <ferozmemon726@gmail.com>
2023-12-25 23:21:45 +0500

    checking

---
+++ app.js
@@ -0,0 +1,8 @@
+let result = confirm();
+if (result === true) {
+    // Action to take if the user clicked "OK"
+    console.log("User wants to proceed.");
+} else {
+    // Action to take if the user clicked "Cancel"
+    console.log("User cancelled the action.");
+}
